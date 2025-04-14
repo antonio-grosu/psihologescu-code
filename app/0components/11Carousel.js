cb81529b030904ee13a427405d98401f29753804
Tirdea Dominic <dominic.tirdea2@gmail.com>
2025-04-14 20:43:59 +0300

    I did the carousel component, i just need to add an animation

--- app/0components/11Carousel.js
+++ app/0components/11Carousel.js
@@ -1,30 +1,139 @@
 "use client";
-import { useEffect } from "react";
+import { useState } from "react";
 import CarouselElement from "./12CarouselElemet";
 
 export default function Carousel() {
+  let cercuri = [];
+  let carouselText = [
+    "Lorem ipsum dolor sit amet consectetur adipiscing elit. Ex sapien vitae pellentesque sem placerat in id. Pretium tellus duis convallis tempus leo eu aenean. Urna tempor pulvinar vivamus fringilla lacus nec metus. Iaculis massa nisl malesuada lacinia integer nunc posuere. Semper vel class aptent taciti sociosqu ad litora. Conubia nostra inceptos himenaeos orci varius natoque penatibus. Dis parturient montes nascetur ridiculus mus donec rhoncus. Nulla molestie mattis scelerisque maximus eget fermentum odio. Purus est efficitur laoreet mauris pharetra vestibulum fusce.",
+    "da",
+    "Lorem ipsum dolor sit amet consectetur adipiscing elit. Ex sapien vitae pellentesque sem placerat in id. Pretium tellus duis convallis tempus leo eu aenean. Urna tempor pulvinar vivamus fringilla lacus nec metus. Iaculis massa nisl malesuada lacinia integer nunc posuere. Semper vel class aptent taciti sociosqu ad litora. Conubia nostra inceptos himenaeos orci varius natoque penatibus. Dis parturient montes nascetur ridiculus mus donec rhoncus. Nulla molestie mattis scelerisque maximus eget fermentum odio. Purus est efficitur laoreet mauris pharetra vestibulum fusce.",
+    "cacat",
+  ];
+
+  for (let i = 0; i < carouselText.length; i++) {
+    cercuri.push(i);
+  }
+  const [displayOption, setDisplayOption] = useState(0);
+
+  function switchRight() {
+    setDisplayOption(displayOption + 1);
+    console.log(displayOption);
+  }
+  function switchLeft() {
+    setDisplayOption(displayOption - 1);
+    console.log(displayOption);
+  }
+  function changeValue(numar) {
+    setDisplayOption(numar);
+  }
+
   return (
-    <div className="w-full flex flex-col overflow-scroll">
-      <CarouselElement
-        numar={2}
-        text="Lorem ipsum dolor sit amet consectetur adipiscing elit. Ex sapien vitae pellentesque sem placerat in id. Pretium tellus duis convallis tempus leo eu aenean. Urna tempor pulvinar vivamus fringilla lacus nec metus. Iaculis massa nisl malesuada lacinia integer nunc posuere. Semper vel class aptent taciti sociosqu ad litora. Conubia nostra inceptos himenaeos orci varius natoque penatibus. Dis parturient montes nascetur ridiculus mus donec rhoncus. Nulla molestie mattis scelerisque maximus eget fermentum odio. Purus est efficitur laoreet mauris pharetra vestibulum fusce."
-      />
-      {/* aici fac divul care tine "punctele" */}
-      <div>
-        <svg
-          className="w-[48px] h-[48px] text-gray-800 dark:text-white"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-        >
-          <path
-            stroke="black"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-          />
-        </svg>
+    <div className="w-full bg-neutral-100 py-16 h-[800px] flex justify-between items-center">
+      <div className="min-w-16 min-h-16 ml-8">
+        {displayOption > 0 && (
+          <svg
+            onClick={switchLeft}
+            className="w-16 h-16 cursor-pointer text-gray-800 dark:text-white"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            width="80"
+            height="80"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <path
+              stroke="black"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M5 12h14M5 12l4-4m-4 4 4 4"
+            />
+          </svg>
+        )}
+      </div>
+      <div className="w-full flex flex-col items-center justify-center relative h-full">
+        {/* aici tin elementele din carusel */}
+        <div className={`w-full flex`}>
+          {carouselText.map((value, index) => {
+            return (
+              <div
+                key={index}
+                className={`w-full ${
+                  displayOption == index ? "flex" : "hidden"
+                }`}
+              >
+                <CarouselElement text={value} />
+              </div>
+            );
+          })}
+        </div>
+        <div className="flex gap-4 absolute left-1/2 -translate-x-1/2 bottom-0">
+          {cercuri.map((value) => {
+            if (value != displayOption) {
+              return (
+                <svg
+                  onClick={() => changeValue(value)}
+                  className="w-4 h-4 text-gray-800 dark:text-white cursor-pointer"
+                  key={value}
+                  id={value}
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    stroke="black"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                  />
+                </svg>
+              );
+            } else {
+              return (
+                <svg
+                  className="w-4 h-4 text-gray-800 dark:text-white"
+                  key={value}
+                  id={value}
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    fill="black"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                  />
+                </svg>
+              );
+            }
+          })}
+        </div>
+      </div>
+      <div className="min-w-16 min-h-16 mr-8">
+        {displayOption < carouselText.length - 1 && (
+          <svg
+            onClick={switchRight}
+            className="w-16 h-16 cursor-pointer text-gray-800 dark:text-white"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            width="80"
+            height="80"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <path
+              stroke="black"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M19 12H5m14 0-4 4m4-4-4-4"
+            />
+          </svg>
+        )}
       </div>
     </div>
   );
--- app/0components/12CarouselElemet.js
+++ app/0components/12CarouselElemet.js
@@ -1,65 +1,9 @@
-export default function CarouselElement({ text, numar }) {
-  let cercuri = [];
-  for (let i = 0; i < numar; i++) {
-    cercuri.push(i);
-  }
+export default function CarouselElement({ text }) {
   return (
-    <div className="w-full bg-indigo-100 flex flex-col  items-center py-24">
-      <div className="flex justify-around items-center">
-        <svg
-          className="w-18 h-18 text-gray-800 dark:text-white"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          width="80"
-          height="80"
-          fill="none"
-          viewBox="0 0 24 24"
-        >
-          <path
-            stroke="black"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M5 12h14M5 12l4-4m-4 4 4 4"
-          />
-        </svg>
-        <p className="w-1/3 text-xl">{text}</p>
-        <svg
-          className="w-18 h-18 text-gray-800 dark:text-white"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          width="80"
-          height="80"
-          fill="none"
-          viewBox="0 0 24 24"
-        >
-          <path
-            stroke="black"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M19 12H5m14 0-4 4m4-4-4-4"
-          />
-        </svg>
-      </div>
-      {cercuri.map((value) => {
-        return (
-          <svg
-            className="w-[48px] h-[48px] text-gray-800 dark:text-white"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-          >
-            <path
-              stroke="black"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-            />
-          </svg>
-        );
-      })}
+    <div className="min-w-full px-16 bg-netrual-100 gap-16 flex justify-center items-center py-40 ">
+      <p className="w-1/2 text-md font-medium text-center leading-[2em]">
+        {text}
+      </p>
     </div>
   );
 }
